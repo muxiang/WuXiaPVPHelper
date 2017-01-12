ea82d7435bbeb5b3b30619c51fc470f28954e38e
muxiang <muxiang1992@outlook.com>
2017-01-12 17:16:00 +0800

    beta2

--- WuXiaPVPHelper/UnitTestProject1/UnitTest1.cs
+++ WuXiaPVPHelper/UnitTestProject1/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -10,9 +11,15 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            Keys k;
-            if(Keys.TryParse(((Keys)113).ToString(), out k))
-                Console.WriteLine(k);
+            Bitmap bmp = (Bitmap)Image.FromFile("D:\\spr-2.png");
+
+            Bitmap bmpOutput = new Bitmap(38, 38);
+
+            using (Graphics g = Graphics.FromImage(bmpOutput))
+            {
+                g.DrawImage(bmp, 0, 0, new Rectangle(43, 27, 38, 38), GraphicsUnit.Pixel);
+            }
+            bmpOutput.Save("D:\\丐帮.png");
         }
     }
 }
--- WuXiaPVPHelper/WuXiaPVPHelper/FrmMain.Designer.cs
+++ WuXiaPVPHelper/WuXiaPVPHelper/FrmMain.Designer.cs
@@ -35,14 +35,28 @@
             this.btnStart = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.tbIcons = new System.Windows.Forms.TrackBar();
+            this.picTianXiang = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.tbIcons)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTianXiang)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             this.SuspendLayout();
             // 
             // cmbCareers
             // 
             this.cmbCareers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbCareers.FormattingEnabled = true;
-            this.cmbCareers.Location = new System.Drawing.Point(95, 14);
+            this.cmbCareers.Location = new System.Drawing.Point(303, 79);
             this.cmbCareers.Name = "cmbCareers";
             this.cmbCareers.Size = new System.Drawing.Size(121, 20);
             this.cmbCareers.TabIndex = 1;
@@ -60,7 +74,7 @@
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(12, 93);
+            this.listView1.Location = new System.Drawing.Point(12, 120);
             this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(386, 245);
@@ -71,7 +85,7 @@
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(223, 13);
+            this.btnStart.Location = new System.Drawing.Point(222, 76);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(75, 23);
             this.btnStart.TabIndex = 4;
@@ -83,7 +97,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(13, 56);
+            this.label2.Location = new System.Drawing.Point(13, 83);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(80, 16);
             this.label2.TabIndex = 2;
@@ -93,7 +107,7 @@
             // 
             this.tbIcons.AutoSize = false;
             this.tbIcons.LargeChange = 40;
-            this.tbIcons.Location = new System.Drawing.Point(95, 49);
+            this.tbIcons.Location = new System.Drawing.Point(95, 76);
             this.tbIcons.Maximum = 80;
             this.tbIcons.Minimum = 40;
             this.tbIcons.Name = "tbIcons";
@@ -103,11 +117,74 @@
             this.tbIcons.TickFrequency = 20;
             this.tbIcons.Value = 60;
             // 
+            // picTianXiang
+            // 
+            this.picTianXiang.Location = new System.Drawing.Point(100, 13);
+            this.picTianXiang.Name = "picTianXiang";
+            this.picTianXiang.Size = new System.Drawing.Size(38, 38);
+            this.picTianXiang.TabIndex = 6;
+            this.picTianXiang.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(144, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(38, 38);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(188, 13);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(38, 38);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(232, 13);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(38, 38);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(276, 13);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(38, 38);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(320, 13);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(38, 38);
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(364, 13);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(38, 38);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 350);
+            this.ClientSize = new System.Drawing.Size(503, 377);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.picTianXiang);
             this.Controls.Add(this.tbIcons);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.listView1);
@@ -124,6 +201,13 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmMain_FormClosed);
             this.Load += new System.EventHandler(this.FrmMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.tbIcons)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTianXiang)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,6 +220,13 @@
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TrackBar tbIcons;
+        private System.Windows.Forms.PictureBox picTianXiang;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
     }
 }
 
